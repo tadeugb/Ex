32446eb0bc860d17742869df39d2ce4c5344d339
Dev Bug <tadeugomesrc@gmail.com>
2024-07-16 20:02:07 -0300

    Add estruturta base de estudos

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json; //dotnet add package Newtonsoft.Json --version 13.0.3
+
+namespace Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
